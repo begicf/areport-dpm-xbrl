e91cd01d744d8c1790a99aa24b07321c698b1621
Fuad Begic <fuad.begic@gmail.com>
2020-06-22 14:38:58 +0200

    bug fix

--- src/Render.php
+++ src/Render.php
@@ -34,7 +34,7 @@ class Render
     private $additionalData = [];
 
 
-    public function __construct($tax = null, $lang = null, $additionalData = nullls)
+    public function __construct($tax = null, $lang = null, $additionalData = null)
     {
         $this->tax = $tax;
         $this->lang = $lang;
